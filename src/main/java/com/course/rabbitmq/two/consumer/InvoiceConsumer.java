8f6d32db3261b3d93de102006d8159a0e31d913e
tmaniak85 <tomasz.maniak.85@gmail.com>
2025-09-06 16:27:30 +0200

    5.One Queue + Multiple Message Types (Consumer) (61)

---
+++ src/main/java/com/course/rabbitmq/two/consumer/InvoiceConsumer.java
@@ -0,0 +1,26 @@
+package com.course.rabbitmq.two.consumer;
+
+import com.course.rabbitmq.two.entity.InvoiceCreatedMessage;
+import com.course.rabbitmq.two.entity.InvoicePaidMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RabbitListener(queues = "q.invoice")
+public class InvoiceConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(InvoiceConsumer.class);
+
+    @RabbitHandler
+    public void handleInvoiceCreated(InvoiceCreatedMessage message) {
+        LOG.info("Invoice created: {}", message);
+    }
+
+    @RabbitHandler
+    public void handleInvoicePaid(InvoicePaidMessage message) {
+        LOG.info("Invoice paid: {}", message);
+    }
+}
--- src/main/java/com/course/rabbitmq/two/consumer/MultiplePrefetchConsumer.java
+++ src/main/java/com/course/rabbitmq/two/consumer/MultiplePrefetchConsumer.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
-@Service
+//@Service
 public class MultiplePrefetchConsumer {
 
     private static final Logger LOG = LoggerFactory.getLogger(MultiplePrefetchConsumer.class);
