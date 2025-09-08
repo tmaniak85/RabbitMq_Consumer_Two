1bcd3253dad5c9f2cadb6f8b93a91f43d368d564
tmaniak85 <tomasz.maniak.85@gmail.com>
2025-09-08 21:32:32 +0200

    10.Create Exchanges & Queues From Java Code : Part Two (68)

--- src/main/java/com/course/rabbitmq/two/consumer/AnotherDummyConsumer.java
+++ src/main/java/com/course/rabbitmq/two/consumer/AnotherDummyConsumer.java
@@ -3,6 +3,10 @@ package com.course.rabbitmq.two.consumer;
 import com.course.rabbitmq.two.entity.DummyMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +15,12 @@ public class AnotherDummyConsumer {
 
     private static final Logger LOG = LoggerFactory.getLogger(AnotherDummyConsumer.class);
 
-    @RabbitListener(queues = "q.another-dummy")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "q.auto-dummy", durable = "true"),
+            exchange = @Exchange(name = "x.auto-dummy", type = ExchangeTypes.DIRECT, durable = "true"),
+            key = "routing-key",
+            ignoreDeclarationExceptions = "true"
+    ))
     public void listenDummy(DummyMessage message) {
         LOG.info("{}", message);
     }
